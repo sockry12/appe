7a67caa8a253ef68e52864711b9e4d09419460c7
sockry12 <971008.ed@gmail.com>
2021-01-13 18:12:48 -0600

    new

---
+++ components/Layout.js
@@ -0,0 +1,104 @@
+import { CssBaseline, ThemeProvider, AppBar, Toolbar, Link, Container, Typography, Box, CircularProgress, Badge } from '@material-ui/core';
+import React, { useContext, useEffect } from 'react';
+import { theme, useStyles } from '../utils/styles';
+import Head from 'next/head';
+import NextLink from 'next/link';
+import { CART_RETRIEVE_REQUEST, CART_RETRIEVE_SUCCESS } from '../utils/constants';
+import { Store } from './Store';
+import getCommerce from '../utils/commerce';
+
+export default function Layout({
+    children,
+    commercePublicKey,
+    title = 'Grupo El Valor',
+}){
+    const classes = useStyles();
+    const { state, dispatch} = useContext(Store);
+    const { cart } = state;
+
+    useEffect(() => {
+        const fetchCart = async () => {
+        const commerce = getCommerce(commercePublicKey);
+        dispatch({ type: CART_RETRIEVE_REQUEST });
+        const cartData = await commerce.cart.retrieve();
+        dispatch({ type: CART_RETRIEVE_SUCCESS, payload: cartData });
+    };
+    fetchCart();
+    }, []);
+
+    return(
+        <React.Fragment>
+            <Head>
+                <meta charSet="utf-8"/>
+                <title> {`${title} - Grupo El Valor`}</title>
+                <link rel="icon" href="/favicon.ico"/>
+                <meta
+                name="viewport"
+                content="with=divice-width, initial-scale=1, shrink-to-fit=no"/>
+
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CssBaseline>
+                <AppBar
+                position="static"
+                color="default"
+                elevation={0}
+                className={classes.AppBar}
+                >
+                    <Toolbar className={classes.toolbar}>
+                    <NextLink href='/'>
+                    <Link 
+                    variant='h6'
+                    color='inherit'
+                    noWrap
+                    href='/'
+                    className={classes.toolbarTitle}
+                    >
+                        Grupo El Valor
+                    </Link>
+                    </NextLink>
+                    <nav>
+                        <NextLink href="/cart">
+                            <Link
+                            variant="button"
+                            color="textPrimary"
+                            href="/cart"
+                            className={classes.link} 
+                            >
+                            { cart.loading ? ( 
+                                <CircularProgress/>
+                            ) : cart.data.total_items > 0 ? (
+                                <Badge badgeContent={cart.data.total_items} color="primary">
+                                    cart
+                                </Badge>
+                            ):(
+                                'cart'
+                            )}
+                            </Link>
+                        </NextLink>
+                    </nav>
+                    </Toolbar>
+
+                </AppBar>
+                <Container component='main' className={classes.main}>
+                {children}
+                </Container>
+                <Container maxWidth="md" component="footer">
+                    <Box mt={5}>
+                        <Typography variant="body2" color="textSecondary" align="center">
+                        {'reservado'}
+                        Grupo el valor 2021
+                        {'.'}
+                        </Typography>
+                    </Box>
+
+                </Container>
+                </CssBaseline>
+            </ThemeProvider>
+
+
+
+
+        </React.Fragment>
+    );
+}
